5ce8ce2d8a9d755957245e1f2c67239dee78cce2
Kyle Xie <kylexie0227@gmail.com>
2022-05-27 18:02:56 +0800

    add demo code

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ZujuAppSync",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "ZujuAppSync",
+            targets: ["ZujuAppSync"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+      .package(
+        name: "AWSAppSync",
+        url: "https://github.com/awslabs/aws-mobile-appsync-sdk-ios.git",
+        .upToNextMinor(from: "3.6.1"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "ZujuAppSync",
+            dependencies: []),
+        .testTarget(
+            name: "ZujuAppSyncTests",
+            dependencies: ["ZujuAppSync"]),
+    ]
+)
---
+++ Sources/ZujuAppSync/ZujuAppSync.swift
@@ -0,0 +1,6 @@
+public struct ZujuAppSync {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
